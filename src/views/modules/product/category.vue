c16a013fdd5ced801760dbb3f3226ff43a66e2e4
rkyang <rkyang@outlook.com>
2022-10-08 17:08:12 +0800

    init

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    getCategory () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        console.log('获取成功……', data)
+      })
+    }
+  },
+  created () {
+    this.getCategory()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

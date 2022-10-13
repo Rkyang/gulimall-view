88b3332aaa18adae996cd5a7b5ce103eacadc378
rkyang <rkyang@outlook.com>
2022-10-13 17:10:02 +0800

    完成分类管理的逻辑删除和新增

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,11 @@
 <template>
-  <el-tree :data="category" :props="defaultProps" :expand-on-click-node="false" node-key="catId" show-checkbox>
+  <div>
+    <el-tree :data="category"
+             :props="defaultProps"
+             :expand-on-click-node="false"
+             node-key="catId"
+             :default-expanded-keys="defaultExpanded"
+             show-checkbox>
     <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
         <span>
@@ -19,7 +25,20 @@
           </el-button>
         </span>
       </span>
-  </el-tree>
+    </el-tree>
+
+    <el-dialog title="新增分类" width="20%" :visible.sync="dialogFormVisible">
+      <el-form :model="categoryInfo">
+        <el-form-item label="分类名称">
+          <el-input v-model="categoryInfo.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogClose">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -27,6 +46,15 @@ export default {
   data () {
     return {
       category: [],
+      categoryInfo: {
+        name: '',
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0
+      },
+      defaultExpanded: [],
+      dialogFormVisible: false,
       defaultProps: {
         children: 'child',
         label: 'name'
@@ -43,10 +71,57 @@ export default {
       })
     },
     append (data) {
-      console.log('append', data)
+      this.dialogFormVisible = true
+      this.categoryInfo.parentCid = data.catId
+      this.categoryInfo.catLevel = data.catLevel * 1 + 1
+      this.categoryInfo.sort = data.child.length * 1 + 1
+    },
+    addCategory () {
+      var expanded = this.categoryInfo.parentCid
+      this.$http({
+        url: this.$http.adornUrl('/product/category/save'),
+        method: 'post',
+        data: this.$http.adornData(this.categoryInfo, false)
+      }).then(({data}) => {
+        this.$message({
+          message: '分类添加成功',
+          type: 'success'
+        })
+        this.dialogClose()
+        this.getCategory()
+        this.defaultExpanded = [expanded]
+      })
     },
     remove (node, data) {
-      console.log('remove', node, data)
+      this.$confirm(`将删除分类【${data.name}】, 是否继续?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        var thisData = data
+        var ids = [data.catId]
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(({data}) => {
+          this.$message({
+            message: '分类删除成功',
+            type: 'success'
+          })
+          this.defaultExpanded = [thisData.parentCid]
+          this.getCategory()
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
+    dialogClose () {
+      this.categoryInfo = this.$options.data().categoryInfo
+      this.dialogFormVisible = false
     }
   },
   created () {

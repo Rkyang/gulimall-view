bf1e16b9d22a0886a898e0980a2a2b770bb9c6b9
rkyang <rkyang@outlook.com>
2022-10-14 13:50:30 +0800

    完成分类管理的修改

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -17,6 +17,12 @@
             Append
           </el-button>
           <el-button
+            type="text"
+            size="mini"
+            @click="() => edit(data)">
+            Edit
+          </el-button>
+          <el-button
             v-if="node.childNodes.length === 0"
             type="text"
             size="mini"
@@ -27,15 +33,21 @@
       </span>
     </el-tree>
 
-    <el-dialog title="新增分类" width="20%" :visible.sync="dialogFormVisible">
+    <el-dialog :title="dialogTitle" width="30%" :visible.sync="dialogFormVisible" :close-on-click-modal="false">
       <el-form :model="categoryInfo">
         <el-form-item label="分类名称">
           <el-input v-model="categoryInfo.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="分类图标">
+          <el-input v-model="categoryInfo.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="分类单位">
+          <el-input v-model="categoryInfo.productUnit" autocomplete="off"></el-input>
+        </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogClose">取 消</el-button>
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button type="primary" @click="submitCategory">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -45,13 +57,17 @@
 export default {
   data () {
     return {
+      dialogTitle: '',
       category: [],
       categoryInfo: {
+        catId: '',
         name: '',
         parentCid: 0,
         catLevel: 0,
         showStatus: 1,
-        sort: 0
+        sort: 0,
+        icon: '',
+        productUnit: ''
       },
       defaultExpanded: [],
       dialogFormVisible: false,
@@ -71,10 +87,23 @@ export default {
       })
     },
     append (data) {
+      this.dialogTitle = '新增分类'
       this.dialogFormVisible = true
       this.categoryInfo.parentCid = data.catId
       this.categoryInfo.catLevel = data.catLevel * 1 + 1
       this.categoryInfo.sort = data.child.length * 1 + 1
+    },
+    edit (data) {
+      console.log(data)
+      this.dialogTitle = '编辑分类'
+      // 请求后台获取最新分类数据
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: 'get'
+      }).then(({data}) => {
+        this.categoryInfo = data.category
+      })
+      this.dialogFormVisible = true
     },
     addCategory () {
       var expanded = this.categoryInfo.parentCid
@@ -84,13 +113,38 @@ export default {
         data: this.$http.adornData(this.categoryInfo, false)
       }).then(({data}) => {
         this.$message({
-          message: '分类添加成功',
+          message: '分类新增成功',
           type: 'success'
         })
         this.dialogClose()
         this.getCategory()
         this.defaultExpanded = [expanded]
       })
+    },
+    editCategory () {
+      var expanded = this.categoryInfo.parentCid
+      var {catId, name, icon, productUnit} = this.categoryInfo
+      this.$http({
+        url: this.$http.adornUrl('/product/category/update'),
+        method: 'post',
+        data: this.$http.adornData({catId, name, icon, productUnit}, false)
+      }).then(({data}) => {
+        this.$message({
+          message: '分类编辑成功',
+          type: 'success'
+        })
+        this.dialogClose()
+        this.getCategory()
+        this.defaultExpanded = [expanded]
+      })
+    },
+    submitCategory () {
+      if (this.dialogTitle === '新增分类') {
+        this.addCategory()
+      }
+      if (this.dialogTitle === '编辑分类') {
+        this.editCategory()
+      }
     },
     remove (node, data) {
       this.$confirm(`将删除分类【${data.name}】, 是否继续?`, '提示', {
